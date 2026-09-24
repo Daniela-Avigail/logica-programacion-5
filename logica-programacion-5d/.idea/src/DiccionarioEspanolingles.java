5ff349bb662e8ed61f6403b79ce17ca99d351097
daniela <danielaavigail.varela123@gmail.com>
2026-09-24 15:24:12 -0600

    Ejercicio de logica de programación 5: diccionario

---
+++ logica-programacion-5d/.idea/src/DiccionarioEspanolingles.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
+public class DiccionarioEspanolIngles {
+
+    public static void main(String[] args) {
+        
