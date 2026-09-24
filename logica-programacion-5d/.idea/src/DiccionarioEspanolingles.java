ae13b51ea253bc531e415c1ea10140f4c2000066
daniela <danielaavigail.varela123@gmail.com>
2026-09-24 15:38:00 -0600

    añado palabras en ingles

--- logica-programacion-5d/.idea/src/DiccionarioEspanolingles.java
+++ logica-programacion-5d/.idea/src/DiccionarioEspanolingles.java
@@ -3,7 +3,31 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
-public class DiccionarioEspanolIngles {
+public class DiccionarioEspanolingles {
 
     public static void main(String[] args) {
-        
+
+        //Crear diccionario con 20 palabras
+        HashMap<String, String> diccionario = new HashMap<>();
+        diccionario.put("casa", "house");
+        diccionario.put("gato", "cat");
+        diccionario.put("puerta", "door");
+        diccionario.put("libro", "book");
+        diccionario.put("agua", "water");
+        diccionario.put("guitarra","guitar");
+        diccionario.put("mariposa","butterfly");
+        diccionario.put("leer","read");
+        diccionario.put("juguete","toy");
+        diccionario.put("escritorio","desk");
+        diccionario.put("silla","chair");
+        diccionario.put("perro","dog");
+        diccionario.put("cama","bed");
+        diccionario.put("cielo","sky");
+        diccionario.put("comida","food");
+        diccionario.put("luna", "moon");
+        diccionario.put("ventana", "window");
+        diccionario.put("tiempo", "time");
+        diccionario.put("computadora", "computer");
+        diccionario.put("sol", "sun");
+    }
+}
