3bb23935e85be0f565e8ea661fd046355417239d
daniela <danielaavigail.varela123@gmail.com>
2026-09-24 15:57:08 -0600

    Elijo una posicion al azar y saco la palabra de la lista

--- logica-programacion-5d/.idea/src/DiccionarioEspanolingles.java
+++ logica-programacion-5d/.idea/src/DiccionarioEspanolingles.java
@@ -14,33 +14,55 @@ public class DiccionarioEspanolingles {
         diccionario.put("puerta", "door");
         diccionario.put("libro", "book");
         diccionario.put("agua", "water");
-        diccionario.put("guitarra","guitar");
-        diccionario.put("mariposa","butterfly");
-        diccionario.put("leer","read");
-        diccionario.put("juguete","toy");
-        diccionario.put("escritorio","desk");
-        diccionario.put("silla","chair");
-        diccionario.put("perro","dog");
-        diccionario.put("cama","bed");
-        diccionario.put("cielo","sky");
-        diccionario.put("comida","food");
+        diccionario.put("guitarra", "guitar");
+        diccionario.put("mariposa", "butterfly");
+        diccionario.put("leer", "read");
+        diccionario.put("juguete", "toy");
+        diccionario.put("escritorio", "desk");
+        diccionario.put("silla", "chair");
+        diccionario.put("perro", "dog");
+        diccionario.put("cama", "bed");
+        diccionario.put("cielo", "sky");
+        diccionario.put("comida", "food");
         diccionario.put("luna", "moon");
         diccionario.put("ventana", "window");
         diccionario.put("tiempo", "time");
         diccionario.put("computadora", "computer");
         diccionario.put("sol", "sun");
 
-     //Se incorporan las palabras en español a una lista para elegir al azar
-       ArrayList<String> listaPalabras = new ArrayList<>(diccionario.keySet());
-       Scanner lector = new Scanner(System.in);
-       Random aleatorio = new Random();
+        //Se incorporan las palabras en español a una lista para elegir al azar
+        ArrayList<String> listaPalabras = new ArrayList<>(diccionario.keySet());
+        Scanner lector = new Scanner(System.in);
+        Random aleatorio = new Random();
 
-       int correctas = 0;
-       int incorretas = 0;
-       System.out.println("Traduce al ingles las siguientes palabras");
-       System.out.println();
+        int correctas = 0;
+        int incorrectas = 0;
+        System.out.println("Traduce al ingles las siguientes palabras");
+        System.out.println();
 
-         }
+ //Repito 5 veces
+        for (int i = 1; i <= 5; i++) {
+// Elijo una posición al azar y saco la palabra de la lista
+            // (con remove no se repite la misma palabra)
+            int posicion = aleatorio.nextInt(listaPalabras.size());
+            String palabraEspanol = listaPalabras.remove(posicion);
 
+            String traduccionCorrecta = diccionario.get(palabraEspanol);
+
+            System.out.print(i + ". ¿Cómo se dice \"" + palabraEspanol + "\" en inglés? ");
+            String respuesta = lector.nextLine();
+
+            if (respuesta.trim().equalsIgnoreCase(traduccionCorrecta)) {
+                System.out.println("Correcto!");
+                correctas++;
+            } else {
+                System.out.println("Incorrecto, la respuesta era: " + traduccionCorrecta);
+                incorrectas++;
+            }
+            System.out.println();
+
+        }
+
+    }
 
          }
